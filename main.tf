0f08bbd0c8943cefa17e2918b85fd629b2e1c5b5
ashisbirlasoft <ashisdevops2023@gmail.com>
2024-07-19 19:44:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,11 @@ terraform {
   }
 
  backend "azurerm" {
-    resource_group_name  = "friday-demo-rg"
-    storage_account_name = "sttfstatemgt01"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    resource_group_name  = "AzureMigratePOCRG"
+    storage_account_name = "statefilebucket"
+    container_name       = "tffile"
+    key                  = "dev.terraform.tffile"
+    
   }
 }
 
