06336fc2951810fdac32b80b12bc430dd088a925
ashisbirlasoft <ashisdevops2023@gmail.com>
2024-07-22 13:36:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,23 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "3.38.0"
     }
   }
 
- backend "azurerm" {
-    resource_group_name  = "AzureMigratePOCRG"
-    storage_account_name = "statefilebucket"
-    container_name       = "tffile"
-    key                  = "dev.terraform.tffile"
-    
-  }
-}
-
-
-
-# Create a resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "AzureMigratePOC"
-  location = "West Europe"
-}
+ 
