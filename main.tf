b624b30dca2fb6c1be02422215e9dd302fb5e49b
ashisbirlasoft <ashisdevops2023@gmail.com>
2024-07-22 19:01:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,27 @@
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = "AzureMigrate"
+## Configure the Azure provider
+provider "azurerm" {
+  features {}
+  version = "=2.46.0"  # Replace with your desired version constraint
 }
 
+# Define the Azure resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "AzureMigrate"
+  location = "South India"
+}
 
+# Define the Azure virtual network (VNet)
+resource "azurerm_virtual_network" "vnet" {
+  name                = "azure-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
+# Define a subnet within the VNet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 
