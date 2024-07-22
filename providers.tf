97688a8f90a37b792ce223479fd65103e1b8f7f0
ashisbirlasoft <ashisdevops2023@gmail.com>
2024-07-22 14:25:56 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-}
 provider "azurerm" {
   features {}
+  source  = "hashicorp/azurerm"
+  version = "=2.46.0"
 }
