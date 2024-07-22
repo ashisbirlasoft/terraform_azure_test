aa4b9eb139efc33524416150f332d3a5df7c8d70
ashisbirlasoft <ashisdevops2023@gmail.com>
2024-07-22 13:43:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-variable "rgname" {
-description = "Resource Group Name"
-default = "github-action1"
-type = string
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
 }
 
-variable "location" {
-description = "Azure location"
-default = "East US"
-type = string
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
