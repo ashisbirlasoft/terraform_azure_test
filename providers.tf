d3bba95f583c39ed3da5d5b6375da4905074421b
ashisbirlasoft <ashisdevops2023@gmail.com>
2024-07-23 18:05:52 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,24 @@
-provider "azurerm" {
+/*provider "azurerm" {
   features {}
   version = "~> 2.0"
+}*/
+
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    organization = "CloudQuickLabs"
+    workspaces {
+      name = "AzureLabs"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
 }
